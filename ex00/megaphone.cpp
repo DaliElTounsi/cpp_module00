0934caa43b5f2a3849631cd11df2ac2f128e7b4f
Mohamed <vipled.contact@gmail.com>
2026-01-14 20:01:17 +0100

    ex00: megaphone - clean code with Makefile

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <cctype>
+#include <iostream>
+
+std::string toUpper(const std::string &str)
+{
+	std::string result = str;
+	for (size_t i = 0; i < result.size(); i++)
+		result[i] = std::toupper(result[i]);
+	return (result);
+}
+
+int main(int ac, char **av)
+{
+	if (ac < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 1;
+	}
+	for (int i = 1; i < ac; i++)
+		std::cout << toUpper(av[i]);
+	std::cout << std::endl;
+	return 0;
+}
